eff0bb89537530d4c889bfedb9685c1c2f03d07b
Shubham Tingare <shubhamtingare0661@gmail.com>
2023-05-07 18:52:59 +0530

    online resume builder

---
+++ src/cv/generator/db.java
@@ -0,0 +1,27 @@
+
+package cv.generator;
+
+
+import java.sql.*;
+import javax.swing.*;
+public class db {
+    
+    
+    
+    Connection conn=null;
+    public static Connection java_db(){
+        
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn =DriverManager.getConnection("jdbc:sqlite:C:\\Users\\shubh\\eclipse-workspace\\CV Generator\\cvdatabase.sqlite");
+      
+            return conn;
+           
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+    }
+}
